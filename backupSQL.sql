71b3134567d25e8740474902d8a492c0b4d4e046
nghia <nguyentrongnghia.kt82@gmail.com>
2024-10-02 00:52:22 +0700

    Update backupSQL.sql

--- backupSQL.sql
+++ backupSQL.sql
@@ -16,6 +16,32 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `categories`
+--
+
+DROP TABLE IF EXISTS `categories`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `categories` (
+  `categoryid` int NOT NULL AUTO_INCREMENT,
+  `categoryname` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+  `images` varchar(500) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+  `status` int DEFAULT NULL,
+  PRIMARY KEY (`categoryid`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `categories`
+--
+
+LOCK TABLES `categories` WRITE;
+/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
+INSERT INTO `categories` VALUES (1,'SamSung','01_S24Ultra-Group-KV_PC_0527_final.jpg',1),(2,'Iphone 16','https://cdn-img-v2.mybota.vn/uploadv2/web/82/8269/product/2024/09/20/08/30/1726797000_apple-iphone-16-pro-finish-lineup-240909_big.jpg',1),(11,'nokia','1727802813886.png',1),(13,'rt','1727804120172.jpeg',1);
+/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `roles`
 --
 
@@ -59,7 +85,7 @@ CREATE TABLE `users` (
   PRIMARY KEY (`id`),
   KEY `roleid_idx` (`roleid`),
   CONSTRAINT `roleid` FOREIGN KEY (`roleid`) REFERENCES `roles` (`roleid`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -68,7 +94,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'nghia','123',NULL,'Nguyễn Trọng Nghĩa','nghia7h2gmail.com',NULL,2,NULL),(2,'son','1',NULL,'son ',NULL,NULL,3,NULL),(3,'hoang','0',NULL,NULL,NULL,NULL,1,NULL),(5,'nghia3','1',NULL,'nguyen','nghia1@gmail.com',NULL,1,NULL),(8,'Nguyễn Trọng Nghĩa','thuc',NULL,'1','nghia7hktvn@gmail.com','',3,'2024-09-15 00:00:00'),(9,'Nguyễn Trọng ','thong123',NULL,'1','thuc@gmail.com','',3,'2024-09-15 00:00:00');
+INSERT INTO `users` VALUES (1,'nghia','1',NULL,'Nguyễn Trọng Nghĩa','nghia7h@gmail.com',NULL,2,NULL),(2,'son','1',NULL,'son ','son@gmail.com',NULL,3,NULL),(3,'hoang','1',NULL,'hoang','hoang@gmail.com',NULL,1,NULL),(5,'nghia3','1',NULL,'nguyen','nghia1@gmail.com',NULL,1,NULL),(16,'trongnghia','1',NULL,'Trong Nghia','nghia@gmail.com',NULL,3,NULL),(20,'thuctrong','1',NULL,'Trần Trọng Thức','thuc@gmail.com','',3,'2024-09-15 00:00:00'),(21,'ngocvan','123',NULL,'Ngọc Vân','van@gmail.com','',3,'2024-09-16 00:00:00'),(22,'thong','123',NULL,'Nguyễn Hữu Thông','thong@gmail.com','',3,'2024-09-22 00:00:00');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -81,4 +107,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-09-15 15:34:29
+-- Dump completed on 2024-10-02  0:51:20
